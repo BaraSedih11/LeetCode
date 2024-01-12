1597dcb022d83f2787137d9d7dd5eb8abd1f9449
BaraAlSedih <98843912+BaraSedih11@users.noreply.github.com>
2024-01-12 20:11:45 +0200

    Time: 0 ms (100.00%), Space: 6.7 MB (38.36%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0 ; i < s.size() ; i++){
+            char c = s[i];
+
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else {
+                if(!st.empty()){
+                    char top = st.top();
+
+                    if(c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '[') return false;
+                     st.pop();
+                }
+                else return false;
+            }
+        }
+
+        if(st.empty()) return true;
+        return false;
+    }
+};
