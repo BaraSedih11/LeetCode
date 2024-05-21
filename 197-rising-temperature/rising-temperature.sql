ee1cd1e2466024fe95ce56546d5fd2c67f6d9489
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-05-21 14:13:55 +0300

    Time: 598 ms (39.74%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,9 +1,8 @@
-SELECT DISTINCT
-    CASE 
-        WHEN w2.recordDate - w1.recordDate = 1 AND w2.temperature > w1.temperature THEN w2.id
-        WHEN w1.recordDate - w2.recordDate = 1 AND w1.temperature > w2.temperature THEN w1.id
-    END AS Id
-FROM weather AS w1
-JOIN weather AS w2
-ON w1.id + 1 = w2.id AND ( w1.recordDate - w2.recordDate = 1 AND w1.temperature > w2.temperature )
-OR ( w2.recordDate - w1.recordDate = 1 AND w2.temperature > w1.temperature );
+-- Write your PostgreSQL query statement below
+SELECT w1.id 
+FROM Weather w1,
+     Weather w2
+WHERE w1.temperature > w2.temperature
+    AND w1.recordDate = w2.recordDate + 1 ;
+
+
